1f03abfa38ebd4e4395621f8711e89619d2097f5
Maxim Tretyakov <scryptan@gmail.com>
2020-04-30 18:50:51 +0500

    Сделал бота, он обыгрывает меня((((

---
+++ tic_tak_toe/XOPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by scryptan on 30.04.2020.
+//
+
+#include "XOPlayer.h"
---
+++ tic_tak_toe/XOPlayer.h
@@ -0,0 +1,14 @@
+//
+// Created by scryptan on 30.04.2020.
+//
+
+#ifndef TIC_TAK_TOE_XOPLAYER_H
+#define TIC_TAK_TOE_XOPLAYER_H
+
+
+class XOPlayer {
+
+};
+
+
+#endif //TIC_TAK_TOE_XOPLAYER_H
